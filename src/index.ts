b05c57b61def1a82905c233eb528b88c303ca7ef
Tonina Tičinović <96192639+tonina-ticinovic@users.noreply.github.com>
2024-11-06 15:55:11 +0100

    Add User, Company and maps (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+/// <reference types="@types/google.maps" />
+
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
+
+const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: { lat: 0, lng: 0 },
+});
